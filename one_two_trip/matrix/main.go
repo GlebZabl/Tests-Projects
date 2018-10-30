7a6bd593b3563f1aa2f5fe16c0c4fd976e296d1b
Zabludovsky Gleb <ggglebz@gmail.com>
2018-10-31 02:40:34 +0300

    almost finished

--- one_two_trip/matrix/main.go
+++ one_two_trip/matrix/main.go
@@ -2,43 +2,46 @@ package main
 
 import "fmt"
 
-func main()  {
+func main() {
 
-	a:=[][]int{[]int{1,2,3,4,5},[]int{6,7,8,9,10},[]int{11,12,13,14,15},[]int{16,17,18,19,20},[]int{21,22,23,24,25}}
-	//idx:=readRightUp(a,[2]int{1,1},1)
-	//idx = readLeftDown(a,idx,2)
-	//idx = readRightUp(a,idx,3)
-	//idx := readLeftDown(a,[2]int{0,2},3)
-	//fmt.Print(a[idx[0]][idx[1]])
-	fmt.Println(readRightUp(a,[2]int{3,1},3))
+	a := [][]int{[]int{17, 16, 15, 14, 13}, []int{18, 5, 4, 3, 12}, []int{19, 6, 1, 2, 11}, []int{20, 7, 8, 9, 10}, []int{21, 22, 23, 24, 25}}
+
+	readLeftDown(a, readRightUp(a, readLeftDown(a, readRightUp(a, [2]int{2, 2}, 1),1), 3),3)
 }
 
-func readMatrix(matrix [][]int, courIndexes [2]int,age int)  {
-	courIndexes = readRightUp(matrix, courIndexes,age)
+func readMatrix(matrix [][]int, courIndexes [2]int, age int) {
+	courIndexes = readRightUp(matrix, courIndexes, age)
 	age++
-	courIndexes = readLeftDown(matrix, courIndexes,age)
+	courIndexes = readLeftDown(matrix, courIndexes, age)
 	age++
-	readMatrix(matrix, courIndexes,age)
+	readMatrix(matrix, courIndexes, age)
 }
 
-func readRightUp(matrix [][]int,courIndexes [2]int,age int) [2]int {
+func readRightUp(matrix [][]int, courIndexes [2]int, age int) [2]int {
 
-	for i:=courIndexes[1];i<=courIndexes[1]+age;i++{
+	for i := courIndexes[1]; i <= courIndexes[1]+age; i++ {
 		fmt.Print(" ")
 		fmt.Print(matrix[courIndexes[0]][i])
 	}
-	for i:=courIndexes[0]-1;i>=courIndexes[0]-age;i--{
+	for i := courIndexes[0] - 1; i >= courIndexes[0]-age; i-- {
 		fmt.Print(" ")
 		fmt.Print(matrix[i][courIndexes[1]+age])
 	}
 
-	courIndexes = [2]int{courIndexes[0]-age,courIndexes[1]+(age-1)}
+	courIndexes = [2]int{courIndexes[0] - age, courIndexes[1] + (age - 1)}
 	return courIndexes
 }
 
-func readLeftDown(matrix [][]int,courIndexes [2]int,age int) [2]int {
+func readLeftDown(matrix [][]int, courIndexes [2]int, age int) [2]int {
 
-	courIndexes = [2]int{courIndexes[0]+age,courIndexes[1]-(age-1)}
+	for i := courIndexes[1]; i >= courIndexes[1]-age; i-- {
+		fmt.Print(" ")
+		fmt.Print(matrix[courIndexes[0]][i])
+	}
+	for i := courIndexes[0]+1;i<=courIndexes[0]+age;i++{
+		fmt.Print(" ")
+		fmt.Print(matrix[i][courIndexes[1]-age])
+	}
+	courIndexes = [2]int{courIndexes[0] + (age+1), courIndexes[1] - age}
 	return courIndexes
 }
-
