d925123505e783e478de182d079f4518b47b8a2b
Zabludovsky Gleb <ggglebz@gmail.com>
2019-03-10 06:14:34 +0300

    add config validation

--- rosst/envirement/config/configOptions.go
+++ rosst/envirement/config/configOptions.go
@@ -4,6 +4,7 @@ import (
 	"Tests-Projects/rosst/envirement/errors"
 	"encoding/json"
 	"io/ioutil"
+	"strconv"
 )
 
 type ConfOptions struct {
@@ -18,6 +19,21 @@ type dbOptions struct {
 	DriverName       string `json:"driver_name"`
 }
 
+func (c *ConfOptions) Validate() string {
+	if !(c.LoggerKey == "console" || c.LoggerKey == "file" || c.LoggerKey == "full") {
+		return "invalid config parameter : logger"
+	}
+
+	if len(c.Port) < 2 {
+		return "invalid config parameter : port"
+	}
+	_, err := strconv.Atoi(c.Port[1:])
+	if string(c.Port[0]) != ":" || err != nil {
+		return "invalid config parameter : port"
+	}
+	return ""
+}
+
 func LoadConfig(path string) (*ConfOptions, error) {
 	rawData, err := ioutil.ReadFile(path)
 	if err != nil {
@@ -30,5 +46,8 @@ func LoadConfig(path string) (*ConfOptions, error) {
 		return nil, Errors.New(err)
 	}
 
+	if wrong := config.Validate(); wrong != "" {
+		return nil, Errors.NewWithMessage(wrong)
+	}
 	return config, nil
 }
